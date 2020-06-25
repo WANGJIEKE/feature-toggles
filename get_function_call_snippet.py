27553cf5b86cc72208f409a1e712538230a2f323
Tongjie <tjwang0221@gmail.com>
2020-06-25 13:54:26 -0700

    update get_function_call_snippet.py

--- get_function_call_snippet.py
+++ get_function_call_snippet.py
@@ -11,11 +11,11 @@ import sys
 from typing import Dict, Iterable, List, Set, Tuple, Union
 
 
-# import Path from correct library based on Python version;
-# use builtin pathlib if available; otherwise use the backport version
-if sys.version_info.major >= 3 and sys.version_info.minor >= 4:
+# import Path from pathlib
+# use the backport version if there is no builtin in current Python version
+try:
     from pathlib import Path  # pylint: disable=import-error
-else:
+except ImportError:
     # https://pypi.org/project/pathlib2/
     from pathlib2 import Path  # pylint: disable=import-error
 
@@ -61,26 +61,6 @@ def get_rel_path(p, root):
     e.g. if p is "/a/b/c" and root is "/a", then return "b/c"
     """
     return os.path.relpath(str(p), start=str(root))
-
-
-def find_func_usage_naive(path, root, funcs):
-    # type: (Path, Path, Iterable[str]) -> Dict[str, List[Tuple[str, int str]]]
-    """iterate through file and find all appearance of function name in funcs
-    the structure of returned dict is {'func_name': [('file_rel_path', line_number, line_content), ...]}
-    """
-    d = defaultdict(list)
-    n = 1
-    rel_path = get_rel_path(path, root)
-    try:
-        with path.open() as f:
-            for line in f:
-                for func in funcs:
-                    if func in line:
-                        d[func].append((rel_path, n, line.rstrip('\n')))
-                n += 1
-    except Exception as e:
-        print(f'Exception with type {type(e)} caught when reading file {rel_path}: {e}', file=sys.stderr)
-    return d
 
 
 def find_func_usage_ast(path, root, funcs):
@@ -162,17 +142,10 @@ if __name__ == '__main__':
         funcs = lib_funcs[get_lib_name_from_repo_name(repo_dir.stem)]
         py_files = [py_file for py_file in walk_dir_for_ext(repo_dir, '.py')]
 
-        # usage = functools.reduce(
-        #     merge_func_usage,
-        #     map(lambda py_file: find_func_usage_naive(py_file, repo_dir, funcs), py_files),
-        #     {}
-        # )
-
         usage_ast = functools.reduce(
             merge_func_usage,
             map(lambda py_file: find_func_usage_ast(py_file, repo_dir, funcs), py_files),
             {}
         )
 
-        # func_usage_dict_to_df(usage).to_csv(repo_stats_root / (repo_dir.stem + '.csv'), index=False)
         func_usage_dict_to_df(usage_ast).to_csv(repo_stats_root / (repo_dir.stem + '.ast.csv'), index=False)
